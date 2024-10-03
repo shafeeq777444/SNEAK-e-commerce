e6d16e614cc4d3a5c2410457ea14a4d3ea539cea
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2024-10-03 09:49:59 +0530

    second commmit

--- src/user-authentication/Login.jsx
+++ src/user-authentication/Login.jsx
@@ -2,13 +2,15 @@ import { useNavigate } from "react-router-dom";
 import { Formik, Form, Field } from "formik";
 import { loginValidation } from "./loginValidation";
 import axios from "axios";
-import './login.css';
+
 
 const Login = () => {
-  const color={
-    backgroundColor:'white'
-  }
+  const color = {
+    backgroundColor: 'white'
+  };
+  
   const navigate = useNavigate();
+  
   const initial = {
     email: "",
     password: "",
@@ -33,14 +35,14 @@ const Login = () => {
   };
 
   return (
-    <div style={color}  className="flex h-screen flex-wrap">
+    <div style={color} className="overflow-x-hidden flex h-screen flex-wrap">
       <div className="flex w-full flex-col md:w-1/2">
         <div className="flex justify-center pt-12 md:-mb-24 md:justify-start md:pl-12">
-        <img className="logo" src="/assets/extra/logo.png"/> 
+          <img className="logo w-36 md:ml-[-2rem] md:mt-[-1rem]" src="/assets/extra/logo.png" />
         </div>
-        <div className="lg:w-[21rem] mx-auto my-auto flex flex-col justify-center pl- pt-8 md:justify-start  md:pt-0">
-          <p className="text-left text-3xl font-bold">You’re back </p>
-          <p className="mt-2 text-left text-gray-500"> Yay, please enter your details.</p>
+        <div className="lg:w-[21rem] mx-auto my-auto flex flex-col justify-center pt-8 md:justify-start md:pt-0">
+          <p className="text-left text-3xl font-bold">You’re back</p>
+          <p className="mt-2 text-left text-gray-500">Yay, please enter your details.</p>
 
           <Formik initialValues={initial} validationSchema={loginValidation} onSubmit={handleSubmit}>
             {({ errors }) => (
@@ -70,7 +72,7 @@ const Login = () => {
                 <div className="flex justify-between">
                   <label className="block text-gray-500 font-bold my-4">
                     <Field type="checkbox" className="leading-loose text-pink-600" />
-                    <span className="py-2 text-sm text-gray-600 leading-snug"> Remember Me </span>
+                    <span className="py-2 text-sm text-gray-600 leading-snug">Remember Me</span>
                   </label>
                   <label className="block text-gray-500 font-bold my-4">
                     <a href="#" className="cursor-pointer tracking-tighter text-black border-b-2 border-gray-200 hover:border-gray-400">
@@ -80,7 +82,7 @@ const Login = () => {
                 </div>
                 <button 
                   type="submit" 
-                  className="w-full rounded-lg bg-gray-900 px-4 py-2 text-center text-base font-semibold text-white shadow-md   transition"
+                  className="w-full rounded-lg bg-gray-900 px-4 py-2 text-center text-base font-semibold text-white shadow-md transition"
                 >
                   Log in
                 </button>
@@ -101,9 +103,8 @@ const Login = () => {
           </div>
         </div>
       </div>
-      <div className="pointer-events-none left-[10rem]  relative hidden h-screen select-none bg-black md:block md:w-1/2">
-       
-        <img className="right  -z-1 absolute top-0 h-full w-[80%] object-cover opacity-90" src="/assets/extra/addImage.jpg" alt="Background" />
+      <div className="pointer-events-none left-[10rem] relative hidden h-screen select-none bg-black md:block md:w-1/2">
+        <img className="right -z-1 absolute top-0 h-full w-[80%] object-cover opacity-90" src="/assets/extra/addImage.jpg" alt="Background" />
       </div>
     </div>
   );
--- src/user-authentication/Register.jsx
+++ src/user-authentication/Register.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Formik, Form, Field } from "formik";
 import React from "react";
 import axios from "axios";
-import './Register.css'
+
+
 const Register = () => {
   const navigate = useNavigate();
   const initial = {
@@ -12,9 +13,9 @@ const Register = () => {
     password: "",
     cpassword: "",
   };
-  const color={
-    backgroundColor:'white'
-  }
+  const color = {
+    backgroundColor: 'white'
+  };
 
   const handleSubmit = async (values) => {
     try {
@@ -28,96 +29,88 @@ const Register = () => {
     }
   };
 
-  return (<div style={color} className="register">
-  <div className="img"><img className="reg-left"  src="/assets/extra/logo.png"/></div>
-    <div className="reg-right font-[sans-serif] bg-white max-w-4xl flex items-center justify-between mx-auto md:h-screen p-4">
-      <div className="de md:grid-cols-3 items-center shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-xl overflow-hidden">
-        <div className="max-md:order-1 flex flex-col justify-center space-y-16 max-md:mt-16 min-h-full bg-gradient-to-r ">
-          <div className="reg-image">
-           <div className="im"><img  src="/assets/extra/addImage.jpg"></img></div>
-          </div>
+  return (
+    <div style={color} className="overflow-x-hidden flex h-screen flex-wrap">
+      <div className="flex w-full flex-col md:w-1/2">
+        <div className="flex justify-center pt-12 md:-mb-24 md:justify-start md:pl-12">
+          <img className="logo w-36 md:ml-[-2rem] md:mt-[-1rem]" src="/assets/extra/logo.png" />
         </div>
+        <div className="lg:w-[21rem] mx-auto my-auto flex flex-col justify-center pt-8 md:justify-start md:pt-0">
+          <p className="text-left text-3xl font-bold">Sign Up</p>
+          <p className="mt-2 text-left text-gray-500">Please enter your details to create an account.</p>
 
-        <Formik className="formik" onSubmit={handleSubmit} initialValues={initial} validationSchema={signupValidation}>
-          {({ errors }) => (
-            <Form className="md:col-span-2 w-full py-6 px-6 sm:px-16">
-              <div className="mb-6">
-                <h3 className="text-gray-800 text-2xl font-bold">Create an account</h3>
-              </div>
-
-              <div className="space-y-6">
+          <Formik initialValues={initial} validationSchema={signupValidation} onSubmit={handleSubmit}>
+            {({ errors }) => (
+              <Form className="flex flex-col pt-3 md:pt-8">
+                <div className="flex flex-col pt-4">
                   <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
-                    <Field 
-                      type="text" 
-                      name="name" 
-                      className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none" 
-                      placeholder="Name" 
+                    <Field
+                      type="text"
+                      name="name"
+                      className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                      placeholder="Name"
                     />
                     {errors.name && <small className="text-red-600">{errors.name}</small>}
                   </div>
+                </div>
+
+                <div className="flex flex-col pt-4">
                   <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
-                    <Field 
-                      type="email" 
-                      name="email" 
-                      className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none" 
-                      placeholder="Email" 
+                    <Field
+                      type="email"
+                      name="email"
+                      className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                      placeholder="Email"
                     />
                     {errors.email && <small className="text-red-600">{errors.email}</small>}
                   </div>
+                </div>
 
+                <div className="flex flex-col pt-4">
                   <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
-                    <Field 
-                      type="password" 
-                      name="password" 
-                      className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none" 
-                      placeholder="Password" 
+                    <Field
+                      type="password"
+                      name="password"
+                      className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                      placeholder="Password"
                     />
                     {errors.password && <small className="text-red-600">{errors.password}</small>}
                   </div>
+                </div>
+
+                <div className="flex flex-col pt-4">
                   <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
-                    <Field 
-                      type="password" 
-                      name="cpassword" 
-                      className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none" 
-                      placeholder="Confirm Password" 
+                    <Field
+                      type="password"
+                      name="cpassword"
+                      className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                      placeholder="Confirm Password"
                     />
                     {errors.cpassword && <small className="text-red-600">{errors.cpassword}</small>}
                   </div>
-
-               
-
-                <div className="flex items-center">
-                  <Field
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                  />
-                  <label htmlFor="remember-me" className="ml-3 block text-sm text-gray-800">
-                    I accept the{" "}
-                    <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1">
-                      Terms and Conditions
-                    </a>
-                  </label>
                 </div>
-              </div>
 
-              <div className="!mt-12">
-                <button type="submit" className="w-full py-3 px-4 tracking-wider text-sm rounded-md text-white bg-gray-700 hover:bg-gray-800 focus:outline-none">
-                  Create an account
+                <button type="submit" className="w-full rounded-lg bg-gray-900 px-4 py-2 mt-10 text-center text-base font-semibold text-white shadow-md transition">
+                  Sign Up
                 </button>
-              </div>
-              <p className="text-gray-800 text-sm mt-6 text-center">
-                Already have an account?{" "}
-                <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1">
-                  Login here
-                </a>
-              </p>
-            </Form>
-          )}
-        </Formik>
+              </Form>
+            )}
+          </Formik>
+
+          <div className="py-12 text-center">
+            <p className="whitespace-nowrap text-gray-600">
+              Already have an account?
+              <button onClick={() => navigate("/")} className="underline-offset-4 font-semibold text-gray-900 underline">
+                Log in
+              </button>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className="pointer-events-none left-[10rem] relative hidden h-screen select-none bg-black md:block md:w-1/2">
+        <img className="right -z-1 absolute top-0 h-full w-[80%] object-cover opacity-90" src="/assets/extra/addImage.jpg" alt="Background" />
+      </div>
     </div>
   );
 };
